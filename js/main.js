01b6dd6f67dc71c55b32bcd2e669e9bf5c9b5cc6
[Gaurav Shukla] <[yoursgaurav@yahoo.com]>
2021-12-31 10:58:23 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function toggling() {
+    let navbar = document.querySelector(".navbar");
+
+    if (navbar.style.right != "0px") {
+        navbar.style.right = "0px";
+    } 
+    
+    else {
+        navbar.style.right = "-100%";
+
+    }
+}
+
+
